52dbfb3a323fd3912c998f4ce82f7172a0926d7f
Jomari Estacio <jomarisk8er@gmail.com>
2021-03-16 17:12:04 +0800

    Development done

---
+++ resources/js/components/ChatMessages.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <ul class="chat">
+        <li class="left clearfix" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                        {{ user.id == message.user.id ? '( Me )' : '' }} :
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    props: ['messages', 'user']
+  };
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', [ChatsController::class, 'index']);
+Route::get('messages', [ChatsController::class, 'fetchMessages']);
+Route::post('messages', [ChatsController::class, 'sendMessage']);
